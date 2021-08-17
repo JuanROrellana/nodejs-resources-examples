5accfa23679653f83622c6271bbfdd947e01f521
Juan Ramirez <juanramirez@Juans-MacBook-Pro.local>
2021-08-16 22:49:35 -0600

    Add sequilize

---
+++ sequelize/migrations/20210817041639-init.js
@@ -0,0 +1,97 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('user', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+
+    await queryInterface.createTable('channel', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'user',
+          key: 'id',
+        },
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        // defaultValue: Sequelize.NOW,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        // defaultValue: Sequelize.NOW,
+      },
+    });
+
+    await queryInterface.createTable('video', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      channel_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'channel',
+          key: 'id',
+          allowNull: false,
+        },
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        // defaultValue: Sequelize.NOW,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        // defaultValue: Sequelize.NOW,
+      },
+    });
+
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropAllTables();
+  }
+};
